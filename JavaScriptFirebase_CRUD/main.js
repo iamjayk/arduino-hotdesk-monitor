76032e0b66bd0b35af528ea4170134791bb73e5c
Ajay Kumar <as1anhawk@hotmail.com>
2017-04-13 22:25:01 +1000

    Final JavaScript_Firebase_CRUD_V.1.0

--- JavaScriptFirebase_CRUD/main.js
+++ JavaScriptFirebase_CRUD/main.js
@@ -1,98 +1,48 @@
 var config = {
-  apiKey: "AIzaSyA2nj6p0kSsALodc5-hiy_HXQmSSf3AtWg",
-    authDomain: "ultrasonicdeskmonitor.firebaseapp.com",
-    databaseURL: "https://ultrasonicdeskmonitor.firebaseio.com",
-    projectId: "ultrasonicdeskmonitor",
-    storageBucket: "ultrasonicdeskmonitor.appspot.com",
-    messagingSenderId: "678304956436"
-};
+    apiKey: "AIzaSyC5bXNboOwc8KQKaq-S-ROq02Wsxt_FHSA",
+    authDomain: "ultrasonicfirebase.firebaseapp.com",
+    databaseURL: "https://ultrasonicfirebase.firebaseio.com",
+    projectId: "ultrasonicfirebase",
+    storageBucket: "ultrasonicfirebase.appspot.com",
+    messagingSenderId: "890017485189"
+  };
 
 firebase.initializeApp(config);
 var db = firebase.database();
+var ultrasonicRef = db.ref('/ultrasonic');
 
-// Timestamp
-function reloadPage() {
-    //var d = new Date();
-    //var n = d.toLocaleString();
-    //document.getElementById("Timestamp").innerHTML = n;
-    location.reload();
+
+// Last Snapshot Value of Ultrasonic 01 Database
+
+ultrasonicRef.on('child_added', function(snapshot) {
+  var lastSnap = snapshot.val();
+
+  console.log("US01_updatedValue: " + lastSnap.US01);
+  document.getElementById("Unit1").innerHTML = lastSnap.US01
+  document.getElementById("date1").innerHTML = lastSnap.Date
+  document.getElementById("time1").innerHTML = lastSnap.Time
+});
+
+// Last Snapshot Value of Ultrasonic 02 Database
+ultrasonicRef.on('child_added', function(snapshot) {
+  var lastSnap = snapshot.val();
+
+//  console.log("US02_updatedValue: " + lastSnap.US02);
+//  document.getElementById("Unit2").innerHTML = lastSnap.US02
+//  document.getElementById("date2").innerHTML = lastSnap.Date
+//  document.getElementById("time2").innerHTML = lastSnap.Time
+});
+
+
+// Page Load Animation
+var myVar;
+function pageLoad() {
+  console.log('%c Loading Data....', 'background: #222; color: #bada55');
+    myVar = setTimeout(showPage, 1000);
 }
 
-// CREATE REWIEW
-
-var reviewForm = document.getElementById('reviewForm');
-var date   = document.getElementById('date');
-var time    = document.getElementById('time');
-var hiddenId   = document.getElementById('hiddenId');
-var element = document.getElementById('element');
-
-reviewForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if (!date.value || !time.value || !element.value ) return null
-
-  var id = hiddenId.value || Date.now()
-
-  db.ref('ultrasonic/' + id).set({
-    Date: date.value,
-    Time: time.value,
-    Value: element.value
-  });
-
-  date.value = '';
-  time.value  = '';
-  hiddenId.value = '';
-  element.value = '';
-});
-
-// READ REVEIWS
-
-var reviews = document.getElementById('reviews');
-var reviewsRef = db.ref('/ultrasonic');
-
-reviewsRef.on('child_added', (data) => {
-  var li = document.createElement('li')
-  li.id = data.key;
-  li.innerHTML = reviewTemplate(data.val())
-  reviews.appendChild(li);
-});
-
-reviewsRef.on('child_changed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.innerHTML = reviewTemplate(data.val());
-});
-
-reviewsRef.on('child_removed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.parentNode.removeChild(reviewNode);
-});
-
-reviews.addEventListener('click', (e) => {
-  var reviewNode = e.target.parentNode
-
-  // UPDATE REVEIW
-  if (e.target.classList.contains('edit')) {
-    date.value = reviewNode.querySelector('.date').innerText;
-    time.value  = reviewNode.querySelector('.time').innerText;
-    hiddenId.value = reviewNode.id;
-    element.value = reviewNode.querySelector('.element').innerText;
-  }
-
-  // DELETE REVEIW
-  if (e.target.classList.contains('delete')) {
-    var id = reviewNode.id;
-    db.ref('ultrasonic/' + id).remove();
-  }
-});
-
-// add both buttons for Full CRUD Functionality in reviewTemplate Function
-// <button class='delete'>Delete</button>
-// and <button class='edit'>Edit</button>
-
-function reviewTemplate({date, time, value}) {
-  return `
-    <div class='date'>${date}</div>
-    <div class='time'>${time}</div>
-    <div class='value'>${value}</div>
-  `
-};
+function showPage() {
+  console.log("%c Data Loaded Successfully!", 'background: #222; color: #bada55');
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
